e48375f0420962440a50f0325725fd7c28a271a2
tatobuxrashvili <tatobuxrashvili1@gmail.com>
2024-04-04 18:47:27 +0400

    second commit

---
+++ extra projects/family.py
@@ -0,0 +1,34 @@
+#my family
+
+dad_name = "alexsandre"
+mam_name = "sofio"
+child1 = "tato"
+child2 = "gio"
+
+#boss family
+boss_family = input("who is boss family: ")
+if boss_family == dad_name:
+    print("yes he is")
+else:
+    print("no he is not")
+
+#diasaxlisi ojaxis
+housewife = input("who is houswife: ")
+if housewife == mam_name:
+    print("yes she is ")
+else:
+    print("no she is not")
+
+#vin swavlobs goashi
+academi = input("who studies in georgias number 1 academii: ")
+if academi == child1:
+    print("yes he is")
+else:
+    print("no he is not")
+
+#ojaxis cancara
+work = input("who is working: ")
+if work == child2:
+    print("yes he is")
+else:
+    print("no he is not")
---
+++ extra projects/registered.py
@@ -0,0 +1,36 @@
+while True:
+
+    print("1. sign up ")
+    print("2. login up ")
+    print("3. exit ")
+
+    user_choice = int(input("please enter your choise:  "))
+
+
+    if user_choice == 1:
+
+        print("sign up page")
+        registered_username =  input("please your name: ")
+        registered_surname =  input("please your  surname: ")
+        registered_password = input("please enter new password: ")
+        registered_password2 = input("please enter Same password: ")
+        
+        if registered_password == registered_password2:
+            print("You have successfully registered")
+        else:
+            print("You failed to register")
+
+    elif user_choice == 2:
+        print("login  page")
+        username_input =  input("please enter your name: ")
+        passsword_input = input("please enter your password: ")
+    
+        if username_input == registered_username and passsword_input == registered_password:
+            print("You have successfully logged into your account")
+            break
+        else:
+            print("please try again. you could not log in to your account")
+
+    elif user_choice == 3:
+        print("Thank you for using our program")
+        break
